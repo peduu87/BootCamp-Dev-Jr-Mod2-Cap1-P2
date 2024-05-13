292794b7b1961c75ac27976b9fe41604694a7def
peduu87 <pepe.silva1202@gmail.com>
2024-05-13 12:45:28 -0300

    closes #4 - the four basic operations

--- javascript.js
+++ javascript.js
@@ -1,9 +1,17 @@
-function sum()
+function calc(op)
 {
     var num1 = parseFloat(document.getElementById("input1").value);
     var num2 = parseFloat(document.getElementById("input2").value);
 
-    var result = num1 + num2;
+    var result = 0;
+
+    switch(op)
+    {
+        case '+' : result = num1 + num2; break;
+        case '-' : result = num1 - num2; break;
+        case '*' : result = num1 * num2; break;
+        case '/' : result = num1 / num2; break;
+    }
 
     document.getElementById("output").value = result;
 }
