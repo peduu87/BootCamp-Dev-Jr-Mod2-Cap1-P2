d13d4e3e47a0808c3067b4e6670260b2c13dd620
peduu87 <pepe.silva1202@gmail.com>
2024-05-20 14:05:43 -0300

    closes #5 = history early logic

--- javascript.js
+++ javascript.js
@@ -9,9 +9,13 @@ function calc(op)
     {
         case '+' : result = num1 + num2; break;
         case '-' : result = num1 - num2; break;
-        case '*' : result = num1 * num2; break;
-        case '/' : result = num1 / num2; break;
+        case '*' : result = num1 * num2; op = "×"; break;
+        case '/' : result = num1 / num2; op = "÷"; break;
     }
 
     document.getElementById("output").value = result;
+
+    var newHistory = "<div>" + num1 + " " + op + " " + num2 + " = " + result + "</div>";
+
+    document.getElementById("history").innerHTML = newHistory + document.getElementById("history").innerHTML;
 }
