4573d3e4cb9fac78b46be948eba026ecc3ec1dcd
peduu87 <pepe.silva1202@gmail.com>
2024-05-20 14:48:58 -0300

    closes #6 - finished history

--- javascript.js
+++ javascript.js
@@ -3,6 +3,16 @@ function calc(op)
     var num1 = parseFloat(document.getElementById("input1").value);
     var num2 = parseFloat(document.getElementById("input2").value);
 
+    if (Number.isNaN(num1))
+    {
+        num1 = 0
+    }
+    
+    if (Number.isNaN(num2))
+    {
+        num2 = 0
+    }
+    
     var result = 0;
 
     switch(op)
@@ -13,9 +23,29 @@ function calc(op)
         case '/' : result = num1 / num2; op = "÷"; break;
     }
 
-    document.getElementById("output").value = result;
+    if (Number.isNaN(result))
+    {
+        result = 0
+    }
 
-    var newHistory = "<div>" + num1 + " " + op + " " + num2 + " = " + result + "</div>";
+    if (Number.isFinite(result))
+    {
+        document.getElementById("output").value = result;        
+    }
 
-    document.getElementById("history").innerHTML = newHistory + document.getElementById("history").innerHTML;
+    else
+    {
+        result = "Inválido";
+        document.getElementById("output").value = result;
+    }
+
+    var newHistory = "<p>" + num1 + " " + op + " " + num2 + " = " + result + "</p>";
+    var history = document.getElementById("history");
+
+    history.innerHTML = newHistory + history.innerHTML;
+
+    if (history.children.length > 10)
+    {
+        history.removeChild(history.childNodes[10]);
+    }
 }
